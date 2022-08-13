52643cfc11aaa5da1878f0633134dfd4905261b8
弦間由樹 <gemma.naok@gmail.com>
2022-08-13 09:32:32 +0900

    vue props/emit 勉強

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Hello from './components/Hello.vue';
+const handleEvent = () => {
+  console.log('子コンポーネントからの通知');
+};
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <Hello v-on:notification="handleEvent" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+const emit = defineEmits(['notification']);
+const sendNotification = () => {
+    emit('notification');
+}
+</script>
+
+<template>
+    <h2>子コンポーネント</h2>
+    <button @click="sendNotification">通知</button>
+</template>
